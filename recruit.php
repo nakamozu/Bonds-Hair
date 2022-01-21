f3dd0abdf9bea5c20625313ee97cd8e35c427f7c
nakamozu <mozu.mo16zu@gmail.com>
2022-01-21 19:01:43 +0900

    企業ページ更新

---
+++ recruit.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bonds Hair | 採用ページ</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
+    <link rel="stylesheet" href="./assets/css/recruit-style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+    </header>
+    <main class="main">
+        <button class="back-btn">
+            <a href="index.html">←</a>
+        </button>
+
+        <div class="sect-mv">
+            <img src="./assets/img/外観写真.JPG" alt="" class="recruit-img">
+            <!-- <img src="./assets/img/作業風景2.jpg" alt="" class="recruit-img">
+            <img src="./assets/img/IMG_7725.JPG" alt="" class="recruit-img">
+            <img src="./assets/img/作業風景4.jpg" alt="" class="recruit-img"> -->
+            <div class="sect-mv__company"><h1>株式会社GROW</h1></div>
+        </div>
+
+        <section class="sect-message">
+            <div class="sect-message__inner container">
+                <div class="sect-title">
+                    <h2>代表者メッセージ</h2>
+                </div>
+                <div class="sect-message__flex">
+                    <div class="sect-message__img">
+                        <img src="./assets/img/山脇伸介.JPG" alt="">
+                    </div>
+                    <h3>代表：山脇 伸介</h3>
+                    <p>ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。ここにテキストが入ります。</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="sect-requirements">
+            <div class="sect-requirements__inner container">
+                <div class="sect-title">
+                    <h2>募集要項</h2>
+                </div>
+                <dl class="sect-requirements__flex">
+                    <dt>勤務地：</dt>
+                    <dd>〒731-0138<br>広島県広島市安佐南区祇園5丁目3-12<br>中本ビル1F</dd>
+                    <dt>勤務時間：</dt>
+                    <dd>〇時～〇時</dd>
+                    <dt>仕事内容：</dt>
+                    <dd>テキストテキスト</dd>
+                    <dt>応募資格：</dt>
+                    <dd>テキストテキスト</dd>
+                    <dt>休日：</dt>
+                    <dd>毎週月曜日・第三日曜日・お盆・年末年始</dd>
+                </dl>
+            </div>
+        </section>
+
+        <section class="sect-company">
+            <div class="sect-company__inner container">
+                <div class="sect-title">
+                    <h2>会社概要</h2>
+                </div>
+                <dl class="sect-company__flex">
+                    <dt>会社名：</dt>
+                    <dd>株式会社GROW</dd>
+                    <dt>代表者：</dt>
+                    <dd>山脇 伸介</dd>
+                    <dt>設立：</dt>
+                    <dd>2022年1月●日</dd>
+                    <dt>所在地：</dt>
+                    <dd>〒731-0138<br>広島県広島市安佐南区祇園5丁目3-12<br>中本ビル1F</dd>
+                    <dt>従業員数：</dt>
+                    <dd>3人（アルバイト・パートを除く）</dd>
+                    <dt>T E L：</dt>
+                    <dd>082-555-9580</dd>
+                </dl>
+            </div>
+        </section>
+
+        <section class="sect-form">
+            <div class="sect-form__inner container">
+                <div class="sect-title">
+                    <h2>応募フォーム</h2>
+                </div>
+                <div class="sect-form__flex">
+                    <p>
+                        「話だけでも聞いてみたい」という方でもお気軽に応募してください。<br>
+                        後日折り返しご連絡いたします。
+                    </p>
+                    <div class="sect-form__form">
+                        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScfQkjRUw-kWAS0ICYA-IPNwUD1B81ydRJjFSrsCQXWPAFo1g/viewform?embedded=true" width="1000" height="1000" frameborder="0" marginheight="0" marginwidth="0" scrolling="no"  frameborder="no">読み込んでいます…</iframe>
+                        <!-- <dt><label for="name">お名前</label></dt>
+                        <dd><input type="text" name="name" required></dd>
+                        <dt><label for="name">フリガナ</label></dt>
+                        <dd><input type="text" name="name" required></dd>
+                        <dt><label for="name">電話番号</label></dt>
+                        <dd><input type="tel" name="name" required></dd>
+                        <dt><label for="name">メールアドレス</label></dt>
+                        <dd><input type="email" name="name" required></dd>
+                        <dt><label for="name">その他</label></dt>
+                        <dd><textarea name="comment"></textarea></dd> -->
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer id="footer">
+        <p class="footer__copyright">&copy; 2022 GROW.All Rights Reserved.</p>
+    </footer></body>
+</html>
